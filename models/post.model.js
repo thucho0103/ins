157adf54ea6ab80880cc38aa8afbc01c06b322d7
unknown <thucho1999@gmail.com>
2021-03-17 11:37:37 +0700

    remote atlas

--- models/post.model.js
+++ models/post.model.js
@@ -3,7 +3,17 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://localhost:27017/demo', {useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('momongodb+srv://movie:admin@cluster0-wmjev.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect('momongodb+srv://movie:admin@cluster0-wmjev.gcp.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const connectionString = 'momongodb+srv://movie:admin@cluster0-wmjev.gcp.mongodb.net/test?retryWrites=true&w=majority';
+mongoose
+    .connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+    })
+    .then(() => console.log('Database connected.'))
+    .catch(err => console.log(err));
+
 
 var PostSchema = new mongoose.Schema({
     userId: String,
